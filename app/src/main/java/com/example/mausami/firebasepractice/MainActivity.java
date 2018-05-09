b6ec7ce558db886c5ac1d2b7fb1aa5843e8181be
Mausami <mausamidangi@gmail.com>
2018-05-09 18:24:43 +0530

    Fix default token

--- app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
+++ app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mausami.firebasepractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,26 +27,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String TOKEN = SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext());
+        final String[] TOKEN = {SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext())};
 
         final TextView txtResponse = findViewById(R.id.txtResponse);
         final EditText txtMessage = findViewById(R.id.txtMessage);
+        final EditText txtDeviceToken = findViewById(R.id.txtDeviceToken);
         Button btnSendMessage = findViewById(R.id.btnSendMessage);
+        final String[] message = {""};
 
-        txtResponse.setText("Your Firebase token... \n\n"+TOKEN);
+        txtResponse.setText("Your Firebase token... \n\n"+ TOKEN[0]);
 
-        JsonObject notification = new JsonObject();
-        notification.addProperty("title", "Ankit Thakur");
-        notification.addProperty("body", "Got a new notification from FCM");
+        txtMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
-        final JsonObject json = new JsonObject();
-        json.addProperty("to", TOKEN);
-        json.add("notification", notification);
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                message[0] = s.toString();
+            }
 
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        txtDeviceToken.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    TOKEN[0] = s.toString();
+                }
+                else {
+                    TOKEN[0] = SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
 
         btnSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                JsonObject notification = new JsonObject();
+                notification.addProperty("title", "Got a new notification from FCM");
+                notification.addProperty("body", "Message: "+ message[0]);
+
+                JsonObject json = new JsonObject();
+                json.addProperty("to", TOKEN[0]);
+                json.add("notification", notification);
+
+                txtResponse.setText("Sending notification to... \n\n"+ TOKEN[0]);
                 retrofit2.Call<ResponseBody> loadChanges = APIClient.getClient()
                         .create(APIInterface.class)
                         .sendNotification(json);
@@ -52,8 +87,16 @@ public class MainActivity extends AppCompatActivity {
                 loadChanges.enqueue(new Callback<ResponseBody>() {
                     @Override
                     public void onResponse(retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
-                        txtResponse.setText(response.toString());
-                        txtMessage.setText("");
+                        if(response.code() == 200) {
+                            txtResponse.setText("Notification sent...");
+                            txtResponse.append("\n\nSent to: "+ TOKEN[0]);
+                            txtResponse.append("\n\nMessage: " + message[0]);
+                            txtResponse.append("\n\nResponse code: "+ response.code());
+                            txtMessage.setText("");
+                        }
+                        else {
+                            txtResponse.setText(response.toString());
+                        }
                     }
 
                     @Override
