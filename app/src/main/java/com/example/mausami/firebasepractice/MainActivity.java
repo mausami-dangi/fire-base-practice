a49a0f027239434d935f214791cdd94c9cdd46bd
Mausami <mausamidangi@gmail.com>
2018-05-09 16:49:38 +0530

    Add send notification from Mobile

--- app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
+++ app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.mausami.firebasepractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.mausami.firebasepractice.helpers.SharedPreferencesHelper;
+import com.example.mausami.firebasepractice.retrofit.APIClient;
+import com.example.mausami.firebasepractice.retrofit.APIInterface;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import okhttp3.ResponseBody;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String TOKEN = SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext());
+        final String TOKEN = SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext());
 
         final TextView txtResponse = findViewById(R.id.txtResponse);
         final EditText txtMessage = findViewById(R.id.txtMessage);
@@ -24,10 +33,34 @@ public class MainActivity extends AppCompatActivity {
 
         txtResponse.setText("Your Firebase token... \n\n"+TOKEN);
 
+        JsonObject notification = new JsonObject();
+        notification.addProperty("title", "Ankit Thakur");
+        notification.addProperty("body", "Got a new notification from FCM");
+
+        final JsonObject json = new JsonObject();
+        json.addProperty("to", TOKEN);
+        json.add("notification", notification);
+
+
         btnSendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                retrofit2.Call<ResponseBody> loadChanges = APIClient.getClient()
+                        .create(APIInterface.class)
+                        .sendNotification(json);
 
+                loadChanges.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(retrofit2.Call<ResponseBody> call, Response<ResponseBody> response) {
+                        txtResponse.setText(response.toString());
+                        txtMessage.setText("");
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<ResponseBody> call, Throwable t) {
+                        Log.d("CALL_STATUS", "CALL Fail");
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/mausami/firebasepractice/retrofit/APIInterface.java
+++ app/src/main/java/com/example/mausami/firebasepractice/retrofit/APIInterface.java
@@ -4,10 +4,15 @@ import com.google.gson.JsonObject;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 public interface APIInterface {
+    @Headers({
+            "Content-Type: application/json",
+            "Authorization: key=AAAAJVZ_mbs:APA91bFmCZThQtIu5DACz0I9iMG_7Bp14iZApeouF4jbuN_2BwN66hjwErjrMR2BHCR6F7Kkb_L7QU13NXCQxEn_GRh3LjrHwpy69LoCwgYdSdBjhINs0-Hseq4oHpZZ-MQYt3Zflo_Y"
+    })
     @POST("/fcm/send")
-    Call<ResponseBody> loadChanges(@Query("to") String to, @Query("notification") JsonObject jsonObject);
+    Call<ResponseBody> sendNotification(@Body JsonObject json);
 }
