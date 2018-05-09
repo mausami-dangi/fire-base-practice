635cf3cfc72e3422f4c6345a447eb8c906d59a6a
Mausami <mausamidangi@gmail.com>
2018-05-09 18:55:19 +0530

    Update notification title

--- app/src/main/java/com/example/mausami/firebasepractice/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/mausami/firebasepractice/MyFirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -22,10 +23,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
         //create notification
-        createNotification(remoteMessage.getNotification().getBody());
+        createNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
     }
 
-    private void createNotification( String messageBody) {
+    private void createNotification(String title, String messageBody) {
         Intent intent = new Intent( this , MainActivity. class );
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent resultIntent = PendingIntent.getActivity( this , 0, intent,
@@ -34,7 +35,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri notificationSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this, "CHANNEL_ID")
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Firebase Practice")
+                .setContentTitle(title)
                 .setContentText(messageBody)
                 .setAutoCancel( true )
                 .setSound(notificationSoundURI)
