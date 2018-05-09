e9fe8d4f51a5fab57274f99315d6faac1bf100cd
Mausami <mausamidangi@gmail.com>
2018-05-09 15:17:32 +0530

    Update View

--- app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
+++ app/src/main/java/com/example/mausami/firebasepractice/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mausami.firebasepractice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String TOKEN = SharedPreferencesHelper.getString(SharedPreferencesHelper.FIREBASE_TOKEN, getApplicationContext());
+
+        final TextView txtResponse = findViewById(R.id.txtResponse);
+        final EditText txtMessage = findViewById(R.id.txtMessage);
+        Button btnSendMessage = findViewById(R.id.btnSendMessage);
+
+        txtResponse.setText("Your Firebase token... \n\n"+TOKEN);
+
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
